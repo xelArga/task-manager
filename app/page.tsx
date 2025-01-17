db2267f6094ce8a427cfe2a442c88f3efdbfda1b
Alex Argatoff <alex@Lunar.local>
2025-01-17 13:17:35 -0800

    added a test comment

--- app/page.tsx
+++ app/page.tsx
@@ -87,7 +87,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Go to nextjs.org →
+          Go to nextjs.org sike →
         </a>
       </footer>
     </div>
